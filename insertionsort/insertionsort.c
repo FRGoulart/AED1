c41e063854233176dc999840ba77195aa005d2b7
Filipe Ribeiro Goulart <50119005+FRGoulart@users.noreply.github.com>
2019-10-17 18:24:33 -0300

    Algoritmo insertionsort
    Algoritmo básico insertionsort

--- insertionsort/insertionsort.c
+++ insertionsort/insertionsort.c
@@ -3,21 +3,21 @@
 #include <string.h>
 
 void insertionsort (int *vet, int n) {
-    int temp=0, i,j;
-    for (i=0; i<n;i++){
-        for (j=1; j<n; j++){
-            if (vet[i] > vet[i+1]){
-                temp = vet[i];
-                vet[i] = vet[i+1];
-                vet[i+1] = temp;
+    int  i,j,data;
+    for (i=1; i<n;i++){
+        data = vet[i];
+         j = i-1;
+        while (j >=0 && vet[j] > data){
+                vet[j+1] = vet[j];
+                j--;
             }
+        vet [j+1] = data;
         }
     }
-}
 int main () {
 
 
-int vet[10] = {1,3,2,4,5,6,8,9,7,10};
+int vet[10] = {10,9,8,7,6,5,4,3,2,1};
 int i,j;
     printf ("Testando implementação algoritmo InsertionSort\n");
     insertionsort (vet,10);
